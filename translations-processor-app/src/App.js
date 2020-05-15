9796c03d01dda0a3a5672d29ef853b9cc80d6ca3
Sydney Thomas <sydney.thomas@conichi.com>
2020-05-15 12:37:38 +0200

    create react app

---
+++ translations-processor-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import infinity from "./assets/infinity.gif";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={infinity} className="App-logo" alt="logo" />
+        <p>Sydney Loves Stefan Forever and Ever </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
