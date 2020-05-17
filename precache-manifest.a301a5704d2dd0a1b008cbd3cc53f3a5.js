6505abf3c560592e32368b19e37041b6b127cd03
Stefan Zdrnja <zdrnjabk4@gmail.com>
2020-05-17 14:37:21 +0200

    Updates

---
+++ precache-manifest.a301a5704d2dd0a1b008cbd3cc53f3a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd770c325121233911bae93ec5803f9",
+    "url": "/translations_processor/index.html"
+  },
+  {
+    "revision": "c64815736b926b94cfdc",
+    "url": "/translations_processor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2196b4c0fe02e87b5d9c",
+    "url": "/translations_processor/static/js/2.c8b84346.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/translations_processor/static/js/2.c8b84346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c64815736b926b94cfdc",
+    "url": "/translations_processor/static/js/main.1a2e5e6b.chunk.js"
+  },
+  {
+    "revision": "b0f18d0f1835cd8f01a1",
+    "url": "/translations_processor/static/js/runtime-main.e73f7105.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/translations_processor/static/media/logo.5d5d9eef.svg"
+  }
+]);
